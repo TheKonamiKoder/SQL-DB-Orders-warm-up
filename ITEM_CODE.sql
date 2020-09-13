56328ac503ab7809ec493d42b462a2a8870aba83
KingKewi123 <70813865+KingKewi123@users.noreply.github.com>
2020-09-13 13:12:22 +0100

    Add files via upload

---
+++ ITEM_CODE.sql
@@ -0,0 +1,12 @@
+INSERT INTO ITEM_INFO(ITEM_NAME,ITEM_CODE)
+VALUES
+     ('Lenovo Laptop', 1),
+	 ('Ipad',2),
+	 ('Nokia', 3),
+	 ('Camera', 4),
+	 ('CPU',5),
+	 ('SD Card',6),
+	 ('Coding Book', 7)
+;
+
+SELECT*FROM ITEM_INFO
